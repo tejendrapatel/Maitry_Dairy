c17b745c3e13eb8a50f6fbcb186ecfe5e3cd7e90
tejendrapatel <info.prospectias@gmail.com>
2021-10-23 17:28:48 +0530

    first commit

---
+++ zdairy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZdairyConfig(AppConfig):
+    name = 'zdairy'
---
+++ zdairy/views.py
@@ -0,0 +1,7 @@
+from zdairy.models import *
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+
+def Home(request):
+    return render(request,'index.html')
